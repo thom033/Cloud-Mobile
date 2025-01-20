91ba84abd609072c5583ed8f95ae429e80bfda59
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-20 15:00:40 +0300

    Create firebase-config.js

---
+++ src/config/firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {  getAuth, getFirestore } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkoBDYntsxs_GOfT5Hw4HflA4Hr-3mhOg",
+  authDomain: "first-project-firebase-cd5be.firebaseapp.com",
+  projectId: "first-project-firebase-cd5be",
+  storageBucket: "first-project-firebase-cd5be.firebasestorage.app",
+  messagingSenderId: "534031217579",
+  appId: "1:534031217579:web:432892912d5b751dcdd8ee",
+  measurementId: "G-9VB12NXW1E"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
