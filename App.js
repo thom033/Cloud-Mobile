018da7c462dc5d20e57e19e8734aa5b4a7d954cd
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-20 20:50:07 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import LoginScreen from './src/screen/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screen/Login'; // Votre écran de connexion
+import ImportFileScreen from './src/screen/ImportFile'; // L'écran que vous voulez afficher après une connexion réussie
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <LoginScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="ImportFile" component={ImportFileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
