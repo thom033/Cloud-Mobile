8257f256659d3aa168a390e7affa28dc640d1900
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-20 20:50:53 +0300

    Update firebase-config.js

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
-import {  getAuth, getFirestore } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-
-const firebaseConfig = {
+const conf = {
   apiKey: "AIzaSyBkoBDYntsxs_GOfT5Hw4HflA4Hr-3mhOg",
   authDomain: "first-project-firebase-cd5be.firebaseapp.com",
   projectId: "first-project-firebase-cd5be",
   storageBucket: "first-project-firebase-cd5be.firebasestorage.app",
   messagingSenderId: "534031217579",
-  appId: "1:534031217579:web:432892912d5b751dcdd8ee",
-  measurementId: "G-9VB12NXW1E"
+  appId: "1:534031217579:web:3f4c09d3021d00becdd8ee",
+  measurementId: "G-CKFN0CNLYD"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(conf);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
